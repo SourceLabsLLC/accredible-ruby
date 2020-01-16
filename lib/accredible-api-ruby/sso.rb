de7f128a89a38048733711115f3f76ed3582d46d
Andrei Bondarev <andrei.bondarev13@gmail.com>
2020-01-16 12:32:42 -0500

    Fix Accredible::SSO#generate_recipient_link method

--- lib/accredible-api-ruby/sso.rb
+++ lib/accredible-api-ruby/sso.rb
@@ -1,15 +1,15 @@
 module Accredible
   class SSO
 
-    def self.generate_recipient_link(recipient_id, recipient_email, wallet_view, group_id, credential_id=nil, redirect_to=nil)
-      params = {
-        recipient_id:    recipient_id,
-        recipient_email: recipient_email,
-        wallet_view:     wallet_view,
-        group_id:        group_id
-      }
-      params[:credential_id] = credential_id if !!credential_id
-      params[:redirect_to]   = redirect_to   if !!redirect_to
+    def self.generate_recipient_link(credential_id:nil, recipient_id:nil, recipient_email:nil, wallet_view:nil, group_id:nil, redirect_to:nil)
+      params = {}
+
+      params[:credential_id]   = credential_id   if !credential_id.nil?
+      params[:recipient_id]    = recipient_id    if !recipient_id.nil?
+      params[:recipient_email] = recipient_email if !recipient_email.nil?
+      params[:wallet_view]     = wallet_view     if !wallet_view.nil?
+      params[:group_id]        = group_id        if !group_id.nil?
+      params[:redirect_to]     = redirect_to     if !redirect_to.nil?
 
       uri = SSO.api_end_point
       Accredible.request(uri, :post, params)
--- spec/accredible/sso_spec.rb
+++ spec/accredible/sso_spec.rb
@@ -2,7 +2,7 @@ describe Accredible::SSO do
   let(:sso) { Accredible::SSO }
 
   it "should return sso link" do
-    response = sso.generate_recipient_link('1234', 'example@example.com', true, '123')
+    response = sso.generate_recipient_link(credential_id: '1234', recipient_email: 'example@example.com', wallet_view: true, group_id: '123')
 
     expect(response).to eq("Stubbed Request")
   end
